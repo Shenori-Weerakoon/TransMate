f915240dc086cb9e4fbda6b06bfb0af0effa6b62
shenori <it22150448@my.sliit.lk>
2024-08-27 15:12:39 +0530

    add translator ui to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Translator from './components/Translator';
+import './App.css';
+import './components/styles/main.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Sinhala to English Translator</h1>
+      <div className="App">
       <Translator />
     </div>
+    
   );
-}
+};
 
 export default App;
