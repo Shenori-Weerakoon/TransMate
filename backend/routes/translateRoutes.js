6bd5706bd0eb59c6ac4d1c41c709d4e27443ece5
shenori <it22150448@my.sliit.lk>
2024-08-29 21:42:48 +0530

    setup getTranslation route

--- backend/routes/translateRoutes.js
+++ backend/routes/translateRoutes.js
@@ -3,5 +3,6 @@ const { translateText } = require('../controllers/translateController');
 const router = express.Router();
 
 router.post('/translate', translateText);
+router.get('/translations', getTranslations); // Read
 
 module.exports = router;
