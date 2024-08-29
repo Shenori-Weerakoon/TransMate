b5c23edf1d5b64dbbb9c53e28e1b56b2fcc71f8f
shenori <it22150448@my.sliit.lk>
2024-08-29 23:56:11 +0530

    modify route

--- backend/routes/translateRoutes.js
+++ backend/routes/translateRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { translateText } = require('../controllers/translateController');
+const { getTranslations } = require('../controllers/translateController');
 const router = express.Router();
 
 router.post('/translate', translateText);
