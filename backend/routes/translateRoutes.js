ba91ce3fe7a993b49cea2be590944e2ac3ddf943
shenori <it22150448@my.sliit.lk>
2024-09-06 22:53:25 +0530

    add get, update and delete routes

--- backend/routes/translateRoutes.js
+++ backend/routes/translateRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { translateText } = require('../controllers/translateController');
+const { translateText, getTranslations, updateTranslation, deleteTranslation } = require('../controllers/translateController');
 const router = express.Router();
 
 router.post('/translate', translateText);
+router.get('/translations', getTranslations); // Read
+router.put('/translations/:id', updateTranslation); // Update
+router.delete('/translations/:id', deleteTranslation); // Delete
 
 module.exports = router;
