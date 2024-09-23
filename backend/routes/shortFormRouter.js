b849a0b92e71334da80e6ecde4146340401786b8
Iresh Eranga <iresheranga2020al@gmail.com>
2024-09-23 06:44:17 +0530

    Update shortFormRouter.js

--- backend/routes/shortFormRouter.js
+++ backend/routes/shortFormRouter.js
@@ -4,6 +4,7 @@ const shortFormController = require('../controllers/shortFormController');
 
 router.post('/shortforms', shortFormController.createShortForm);
 router.get('/shortforms', shortFormController.getAllShortForms);
+router.get('/accepted/shortforms', shortFormController.getAllAcceptedShortForms);
 router.get('/shortforms/:shortForm', shortFormController.getShortForm);
 router.put('/shortforms/:shortForm', shortFormController.updateShortForm);
 router.delete('/shortforms/:shortForm', shortFormController.deleteShortForm);
